efb830376fde9b551eb5074567001ff75a1ac994
huangjian <1342042894@qq.com>
2019-09-02 20:45:46 +0800

    modify shape

--- paintdom/shape.go
+++ paintdom/shape.go
@@ -1,1 +1,36 @@
 package paintdom
+
+type coord = float64
+type ShapeID = string
+
+type Shape interface {
+	GetID() ShapeID
+}
+
+type shapeBase struct {
+	ID ShapeID `json:"id"`
+}
+
+func (p *shapeBase) GetID() ShapeID {
+	return p.ID
+}
+
+// ---------------------------------------------------
+
+type ShapeStyle struct {
+	LineWidth coord  `json:"lineWidth"`
+	LineColor string `json:"lineColor"`
+	FillColor string `json:"fillColor"`
+}
+
+type Point struct {
+	X coord `json:"x"`
+	Y coord `json:"y"`
+}
+
+// ---------------------------------------------------
+
+type Path struct {
+}
+
+// ---------------------------------------------------
