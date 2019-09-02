9df24de12d44530690d7323f36dfb6a423b96506
huangjian <1342042894@qq.com>
2019-09-02 20:04:18 +0800

    fix merge

--- paintdom/service.go
+++ paintdom/service.go
@@ -1,8 +1,5 @@
 package paintdom
-<<<<<<< HEAD
 
 func Main() {
 
 }
-=======
->>>>>>> 4089f4170c5ffb5be6ad5b55f4d7494391d6a47c
--- paintweb/main.go
+++ paintweb/main.go
@@ -5,11 +5,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 
-<<<<<<< HEAD
 	"github.com/MDGSF/go-paint/paintdom"
-=======
-	"github.com/qiniu/qpaint/paintdom"
->>>>>>> 4089f4170c5ffb5be6ad5b55f4d7494391d6a47c
 )
 
 func newReverseProxy(baseURL string) *httputil.ReverseProxy {
@@ -19,11 +15,7 @@ func newReverseProxy(baseURL string) *httputil.ReverseProxy {
 
 func handleDefault(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path == "/" {
-<<<<<<< HEAD
 		http.ServeFile(w, req, "www/index.html")
-=======
-		http.ServeFile(w, req, "www/index.htm")
->>>>>>> 4089f4170c5ffb5be6ad5b55f4d7494391d6a47c
 		return
 	}
 	req.URL.RawQuery = "" // skip "?params"
